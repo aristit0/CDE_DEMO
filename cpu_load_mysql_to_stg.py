1260813967d30df30ac1c2adebc94c1d13297c58
aristit0 <aristitosugihharto@gmail.com>
2025-05-11 15:50:27 +0700

    init 2

--- cpu_load_mysql_to_stg.py
+++ cpu_load_mysql_to_stg.py
@@ -9,7 +9,18 @@ properties = {
     "driver": "com.mysql.cj.jdbc.Driver"
 }
 
-df = spark.read.jdbc(url=jdbc_url, table="mobile_transactions", properties=properties)
+df = spark.read.jdbc(
+    url=jdbc_url,
+    table="mobile_transactions",
+    column="transaction_id",            # numeric and indexed column
+    lowerBound=1,
+    upperBound=10000000,
+    numPartitions=10,                   # can be tuned, try 10–20
+    properties=properties
+)
+
+#df = spark.read.jdbc(url=jdbc_url, table="mobile_transactions", properties=properties)
+
 df.write.mode("overwrite").format("parquet").saveAsTable("stg.mobile_transactions")
 
 spark.stop()
